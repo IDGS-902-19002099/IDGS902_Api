8912d9d217fa6c4b10d25deeef383f5a9b1b39ed
IDGS-902-19002099 <djhpadron246@gmail.com>
2023-07-13 23:03:53 -0600

    DJPV clase 06.07.2023

---
+++ IDGS902_Api/Models/alumnos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IDGS902_Api.Models
+{
+    public class Alumnos
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Correo { get; set; }
+
+
+    }
+}
+
